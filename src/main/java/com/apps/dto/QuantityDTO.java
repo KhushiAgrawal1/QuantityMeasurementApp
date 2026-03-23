4fbc7f1eb19f503177c8b9c0b609eba684c188b1
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-03-23 16:38:28 +0530

    [KhushiAgrawal] Added : UC16 Implementation

--- src/main/java/com/apps/dto/QuantityDTO.java
+++ src/main/java/com/apps/dto/QuantityDTO.java
@@ -11,5 +11,23 @@ public class QuantityDTO {
 
     private double value;
     private String unit;
-
+	public QuantityDTO(double value, String unit) {
+		super();
+		this.value = value;
+		this.unit = unit;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+    
+   
 }
